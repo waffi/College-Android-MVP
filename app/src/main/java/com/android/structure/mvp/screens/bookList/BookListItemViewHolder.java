9955855bbed33efcfcd72871ef597433e01dd569
Waffi Fatur Rahman <waffi@xtremax.com>
2019-07-06 08:13:23 +0700

    Implement picasso

--- app/src/main/java/com/android/structure/mvp/screens/bookList/BookListItemViewHolder.java
+++ app/src/main/java/com/android/structure/mvp/screens/bookList/BookListItemViewHolder.java
@@ -2,20 +2,24 @@ package com.android.structure.mvp.screens.bookList;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.structure.mvp.R;
 import com.android.structure.mvp.models.Book;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 class BookListItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     private final BookListContract.Presenter presenter;
     private final TextView bookNameTextView;
-    private final TextView bookDateTextView;
+    private final TextView bookDescriptionTextView;
+    private final ImageView bookIdentifierImageView;
     private Book currentItem;
 
     private BookListItemViewHolder(View itemView, BookListContract.Presenter presenter) {
@@ -24,7 +28,8 @@ class BookListItemViewHolder extends RecyclerView.ViewHolder implements View.OnC
         this.presenter = presenter;
 
         this.bookNameTextView = (TextView) itemView.findViewById(R.id.book_name_textview);
-        this.bookDateTextView = (TextView) itemView.findViewById(R.id.book_date_textview);
+        this.bookDescriptionTextView = (TextView) itemView.findViewById(R.id.book_desc_textview);
+        this.bookIdentifierImageView = (ImageView) itemView.findViewById(R.id.book_identifier_img);
 
         this.itemView.setOnClickListener(this);
     }
@@ -40,8 +45,14 @@ class BookListItemViewHolder extends RecyclerView.ViewHolder implements View.OnC
 
         Context context = this.itemView.getContext();
 
-        this.bookNameTextView.setText(item.title.toString());
-        this.bookDateTextView.setText(item.date.toString());
+        try {
+            this.bookNameTextView.setText(item.title.toString());
+            this.bookDescriptionTextView.setText(item.description.toString());
+            Picasso.with(context).load(((List<String>) item.identifier).get(0)).into(this.bookIdentifierImageView);
+        }
+        catch (Exception e){
+
+        }
     }
 
     @Override
